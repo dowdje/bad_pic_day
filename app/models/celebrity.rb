9bbf98802e970ac5a52b31a463efc2c2147e53b4
Tony Mai <tonymaibox@yahoo.com>
2016-08-26 18:18:21 -0400

    added photo upload capabilities

--- app/models/celebrity.rb
+++ app/models/celebrity.rb
@@ -1,4 +1,6 @@
 class Celebrity < ApplicationRecord
 	has_many :photos
 	has_many :comments, through: :photos
+	has_attached_file :image, styles: { :medium => "640x" }
+	# Validate the file
 end
--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,4 +1,7 @@
 class Photo < ApplicationRecord
   belongs_to :celebrity
   has_many :comments
+
+  	has_attached_file :image, styles: { :medium => "640x" }
+	validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 end
