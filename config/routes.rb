f6eb049fec41a860f32b59c789ede7c22cad4336
Tony Mai <tonymaibox@yahoo.com>
2016-08-28 14:50:53 -0400

    added root_path to routes, took out resources: sessions

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,16 @@
 Rails.application.routes.draw do
   
-  resources :users
+  
   resources :registrations
-  resources :sessions
-  get '/', to: 'application#index'
+  # resources :sessions
+  get '/sessions/new', to: 'sessions#new'
+  post '/sessions/new', to: 'sessions#create', as: 'new_session'
+  get '/', to: 'application#index', as: 'root'
 
   resources :photos
   resources :comments, only: [:create, :edit, :update, :destroy]
   resources :celebrities
-
+  resources :users
   # post '/', to: 'sessions#create', as: 'login'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
