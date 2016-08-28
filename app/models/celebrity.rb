cff760909caac710a2154fcb65bab08c353b11bd
dowdje <dowdje89@gmail.com>
2016-08-27 20:05:31 -0400

    Added autosave method and custom validator to Photo:celebrities

--- app/models/celebrity.rb
+++ app/models/celebrity.rb
@@ -2,6 +2,5 @@ class Celebrity < ApplicationRecord
 	has_many :photos
 	has_many :comments, through: :photos
 	has_attached_file :image, styles: { :medium => "640x" }
-  validates_uniqueness_of :name
 	# Validate the file
 end
